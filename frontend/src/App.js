1c1b21f24f1564f33217d76ef81fc5b2d7aca7c2
Meenesh Kapadia <mkap@KapBook-Air.localdomain>
2024-07-31 16:40:14 -0700

    formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import AgeInput from './AgeInput';
 import HeightInput from './HeightInput';
 import WeightInput from './WeightInput';
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 
 function App() {
   const [age, setAge] = useState('');
@@ -20,20 +22,20 @@ function App() {
   return (
     <Container>
       <h1 style={{ textAlign: 'center' }}>Macronutrients Calculator</h1>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={4}>
-          <GenderSelector gender={gender} setGender={setGender} />
+      <Grid container spacing={3} justifyContent="center">
+        <Grid item xs={12} md={6}>
+          <Box display="flex" flexDirection="column" alignItems="stretch" gap={2}>
+            <GenderSelector gender={gender} setGender={setGender} />
+            <AgeInput age={age} setAge={setAge} />
+            <HeightInput height={height} setHeight={setHeight} />
+            <WeightInput weight={weight} setWeight={setWeight} />
+          </Box>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <AgeInput age={age} setAge={setAge} />
+        <Grid item xs={12} style={{ textAlign: 'center', marginTop: '20px' }}>
+          <Button variant="contained" color="primary" onClick={calculateMacronutrients}>
+            Calculate
+          </Button>
         </Grid>
-        <Grid item xs={12} md={4}>
-          <HeightInput height={height} setHeight={setHeight} />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <WeightInput weight={weight} setWeight={setWeight} />
-        </Grid>
-        <button onClick={calculateMacronutrients}>Calculate</button>
       </Grid>
     </Container>
   );
--- frontend/src/GenderSelector.js
+++ frontend/src/GenderSelector.js
@@ -1,29 +1,30 @@
-import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import Grid from '@mui/material/Grid';
+import React from 'react';
+import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Grid, Box } from '@mui/material';
 
-export default function GenderSelector({ gender, setGender }) {
-  const handleChange = (event) => {
+function GenderSelector({ gender, setGender }) {
+  const handleGenderChange = (event) => {
     setGender(event.target.value);
   };
 
   return (
-    <FormControl fullWidth>
-      <FormLabel id="gender-radio-buttons-group-label">Gender</FormLabel>
-      <RadioGroup
-        aria-labelledby="gender-radio-buttons-group-label"
-        value={gender}
-        onChange={handleChange}
-        name="radio-buttons-group"
-        row
-      >
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-      </RadioGroup>
+    <FormControl component="fieldset">
+      <Box display="flex" alignItems="center">
+        <Box marginRight={2}>
+          <FormLabel component="legend">Gender</FormLabel>
+        </Box>
+        <RadioGroup
+          row
+          aria-label="gender"
+          name="gender"
+          value={gender}
+          onChange={handleGenderChange}
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+        </RadioGroup>
+      </Box>
     </FormControl>
   );
 }
+
+export default GenderSelector;
