861266278bdb2409bc8262efd217e75eeec456ef
Meenesh Kapadia <mkap@KapBook-Air.localdomain>
2024-08-05 18:36:21 -0700

    updating CI and local testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,18 @@
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routers import macronutrients
 
 app = FastAPI()
 
-# Include the macronutrients router with an optional prefix
-app.include_router(macronutrients.router, prefix="/api")
+# Use an environment variable to determine the directory
+frontend_directory = os.getenv('FRONTEND_DIRECTORY', 'frontend/build')
 
 # Serve the frontend static files
-app.mount("/", StaticFiles(directory="../frontend/build", html=True), name="static")
+app.mount("/", StaticFiles(directory=frontend_directory, html=True), name="static")
+
+# Include the macronutrients router
+app.include_router(macronutrients.router)
 
 if __name__ == "__main__":
     import uvicorn
